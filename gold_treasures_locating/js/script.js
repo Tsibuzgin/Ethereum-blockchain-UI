4392125f5ab98ff16d536a8bdd18e0d579dd9d8f
Vladymyr Tsybuzghin <periodical7rio@gmail.com>
2022-05-09 13:42:58 +0300

    init

--- gold_treasures_locating/js/script.js
+++ gold_treasures_locating/js/script.js
@@ -8,60 +8,62 @@ function id (x, y) {
 
 // actual  conversion code starts here
 
-var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-var teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+// var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+// var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+// var teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
-function convert_millions(num) {
-  if (num >= 1000000) {
-    return convert_millions(Math.floor(num / 1000000)) + " million " + convert_thousands(num % 1000000);
-  } else {
-    return convert_thousands(num);
-  }
-}
+// function convert_millions(num) {
+//   if (num >= 1000000) {
+//     return convert_millions(Math.floor(num / 1000000)) + " million " + convert_thousands(num % 1000000);
+//   } else {
+//     return convert_thousands(num);
+//   }
+// }
 
-function convert_thousands(num) {
-  if (num >= 1000) {
-    return convert_hundreds(Math.floor(num / 1000)) + " thousand " + convert_hundreds(num % 1000);
-  } else {
-    return convert_hundreds(num);
-  }
-}
+// function convert_thousands(num) {
+//   if (num >= 1000) {
+//     return convert_hundreds(Math.floor(num / 1000)) + " thousand " + convert_hundreds(num % 1000);
+//   } else {
+//     return convert_hundreds(num);
+//   }
+// }
 
-function convert_hundreds(num) {
-  if (num > 99) {
-    return ones[Math.floor(num / 100)] + " hundred " + convert_tens(num % 100);
-  } else {
-    return convert_tens(num);
-  }
-}
+// function convert_hundreds(num) {
+//   if (num > 99) {
+//     return ones[Math.floor(num / 100)] + " hundred " + convert_tens(num % 100);
+//   } else {
+//     return convert_tens(num);
+//   }
+// }
 
-function convert_tens(num) {
-  if (num < 10) return ones[num];
-  else if (num >= 10 && num < 20) return teens[num - 10];
-  else {
-    return tens[Math.floor(num / 10)] + " " + ones[num % 10];
-  }
-}
+// function convert_tens(num) {
+//   if (num < 10) return ones[num];
+//   else if (num >= 10 && num < 20) return teens[num - 10];
+//   else {
+//     return tens[Math.floor(num / 10)] + " " + ones[num % 10];
+//   }
+// }
 
-function convert(num) {
-  if (num == 0) return "zero";
-  else return convert_millions(num);
-}
+// function convert(num) {
+//   if (num == 0) return "zero";
+//   else return convert_millions(num);
+// }
 
-//end of conversion code
+// //end of conversion code
 
-//testing code begins here
+// //testing code begins here
 
-function main(a, b) {
-    let cases = id(a, b);
-    if (cases === 'Error') {
-        console.log(cases);
-        cases = null;
-    } else {
-        console.log(convert(cases));
-    } 
-}
+// function solution(a, b) {
+//     let cases = id(a, b);
+//     if (cases === 'Error') {
+//         console.log(cases);
+//         cases = null;
+//     } else {
+//         console.log(convert(cases));
+//     };
+//     return(convert(cases));
+// }
 
-//enter your x and y as arguments to main
-main(0,1);
+//enter your x and y as arguments to id
+console.log(id(7,1).toString(10));
+
